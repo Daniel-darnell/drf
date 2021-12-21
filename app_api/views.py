30f2b2f3a85959ecb1d1cc86540b19ea74d3da7f
Daniel-darnell <darnelldanny1997@gmail.com>
2021-12-21 17:40:17 +0300

    Created first View item

--- app_api/views.py
+++ app_api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from .serializers import CartItemSerializers
+from .models import CartItem
 
 # Create your views here.
+class CartItemViews(APIView):
+    def post(self, request):
+        serializer = CartItemSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response ({"status":"success", "data":serializer.data}, status=status.HTTP_200_ok)
+        else:
+            return Response({"status":"error", "data":serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
